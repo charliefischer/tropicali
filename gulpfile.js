a707864450ca7d668f10e3bed459334d5d4b9304
Charlie Fischer <charlie.a.fischer@gmail.com>
2020-09-30 15:45:35 +0100

    optimized version of the landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,63 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCSS = require("gulp-clean-css")
+var sourceMaps = require("gulp-sourcemaps")
 
-gulp.task('default', async function() {
-  // place code for your default task here
-});
+var browserSync = require("browser-sync").create()
+
+var imageMin = require("gulp-imageMin")
+
+
+sass.compiler = require('node-sass')
+
+gulp.task("sass", function(){
+
+    return gulp.src("src/css/app.scss")
+
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(
+        cleanCSS({
+            compatibility: 'ie8'
+        })
+    )
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream())
+
+})
+
+gulp.task("html", function(){
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("fonts", function () {
+    return gulp.src("src/fonts/*")
+        .pipe(gulp.dest("dist/fonts"))
+})
+
+gulp.task("images", function () {
+    return gulp.src("src/img/*")
+        .pipe(imageMin())
+        .pipe(gulp.dest("dist/img"))
+})
+
+
+gulp.task("watch", function(){
+
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    })
+
+    gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
+    gulp.watch("src/css/app.scss", ["sass"])
+    gulp.watch("src/fonts/*", ["fonts"])
+    gulp.watch("src/img/*", ["images"])
+})
+
+
+gulp.task('default', ["html", "sass", "watch", "fonts", "images"]
+);
